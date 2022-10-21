91ead42c8e5b02bf7f5de1dce7129f62f0ad1f7d
Melina <melinalarramendy@gmail.com>
2022-10-21 19:29:21 -0300

    Se le dió funcionalidad al boton resumen dandole utilidad al form de compra

---
+++ scripts.js
@@ -0,0 +1,54 @@
+const nombreOrador = document.getElementById("nombre");
+
+const apellidoOrador = document.getElementById("apellido");
+
+const inputCategoria = document.getElementById("inputCategoria");
+
+const cantidadCompra = document.getElementById("cantidadCompra");
+
+const mensajeNombre = document.getElementById("mensajeNombre");
+
+const mensajeEmail = document.getElementById("mensajeEmail");
+
+const totalValorh5 = document.getElementById("totalValor");
+
+const botonOrador = document.getElementById("botonOrador");
+
+const botonResumen = document.getElementById("botonResumen");
+
+const email = document.getElementById("email");//compra
+
+const expRegular = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+
+
+function validarNombre() {
+    if (nombreOrador.value.length < 3) {
+        mensajeNombre.innerHTML = "Ingrese un nombre válido.";
+        
+    } else {
+        mensajeNombre.innerHTML = "Nombre correcto!";
+    }
+} 
+
+function validarEmail() {
+    if(!expRegular.test(email.value)) {
+        mensajeEmail.innerHTML = "El email no es válido."
+    } else {
+        mensajeEmail.innerHTML = "Email correcto!"
+    }
+}
+
+function totalPagar() {
+    let precio = 200;
+    if(inputCategoria.value == 1) {
+        totalValorh5.innerHTML = "Total a pagar: $ "+ (precio*cantidadCompra.value)*80/100;
+    } else if (inputCategoria.value == 2) {
+        totalValorh5.innerHTML = "Total a pagar: $ "+ (precio*cantidadCompra.value)*50/100;
+    } else if (inputCategoria.value == 3) {
+        totalValorh5.innerHTML = "Total a pagar: $ "+ precio - (precio*cantidadCompra.value)*15/100;
+    }
+}
+
+botonOrador.addEventListener("click", validarNombre);
+
+botonResumen.addEventListener("click", totalPagar);
