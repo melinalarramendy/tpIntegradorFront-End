d656c8932b96c76a398267f25062be570f1a27db
Melina <melinalarramendy@gmail.com>
2022-11-07 17:35:39 -0300

    correccion de error botones en js

--- scripts.js
+++ scripts.js
@@ -21,6 +21,9 @@ const botonResumen = document.getElementById("botonResumen");
 
 const expRegular = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/
 
+var verificaNombre;
+var verificaApellido;
+var verificaMail;
 
 function validarNombreOrador() {
     if (nombreOrador.value.length < 3) {
@@ -32,15 +35,18 @@ function validarNombreOrador() {
 function validarApellidoOrador() {
     if (apellidoCompra.value.length < 4) {
         mensajeApellidoOrador.innerHTML = "Ingrese un apellido válido.";
-    }
+
+    } 
 }
 
 function validarNombreCompra() {
     if (nombreCompra.value.length < 3) {
         nombreCompra.style.border ="1px solid red";
         nombreCompra.placeholder = "Nombre incorrecto!";
+        verificaNombre = false;
     } else {
         nombreCompra.style.border ="1px solid green";
+        verificaNombre = true;
     }
 } 
 
@@ -48,8 +54,10 @@ function validarApellidoCompra() {
     if (apellidoCompra.value.length < 4) {
         apellidoCompra.style.border ="1px solid red";
         apellidoCompra.placeholder = "Apellido incorrecto!";
+        verificaApellido = false;
     } else {
         apellidoCompra.style.border ="1px solid green";
+        verificaApellido = true;
     }
 } 
 
@@ -57,27 +65,40 @@ function validarEmail() {
     if(!expRegular.test(emailCompra.value)) {
         emailCompra.style.border ="1px solid red";
         emailCompra.placeholder = "Correo incorrecto!";
+        verificaMail = false;
     } else {
         emailCompra.style.border ="1px solid green";
+        verificaMail = true;
     }
 }
 
 
 
 function totalPagar() {
+
     let precio = 200;
-    if(inputCategoria.value == 1) {
-        totalValorh5.innerHTML = "Total a pagar: $ "+ (precio*cantidadCompra.value)*80/100;
-    } else if (inputCategoria.value == 2) {
-        totalValorh5.innerHTML = "Total a pagar: $ "+ (precio*cantidadCompra.value)*50/100;
-    } else if (inputCategoria.value == 3) {
-        totalValorh5.innerHTML = "Total a pagar: $ "+ precio - (precio*cantidadCompra.value)*15/100;
+    if (verificaNombre == true && verificaApellido == true && verificaMail == true) {
+        if(inputCategoria.value == 1) {
+            totalValorh5.innerHTML = "Total a pagar: $ "+ (precio*cantidadCompra.value)*80/100;
+        } else if (inputCategoria.value == 2) {
+            totalValorh5.innerHTML = "Total a pagar: $ "+ (precio*cantidadCompra.value)*50/100;
+        } else if (inputCategoria.value == 3) {
+            totalValorh5.innerHTML = "Total a pagar: $ "+ precio - (precio*cantidadCompra.value)*15/100;
+        }
     }
+    
 }
 
 function borrar() {
     totalValorh5.innerHTML = "Total a pagar: $ ";
     formulario.reset(); 
+    nombreCompra.style.border ="1px solid grey";
+    nombreCompra.placeholder = "Nombre";
+    apellidoCompra.style.border ="1px solid grey";
+    apellidoCompra.placeholder = "Apellido";
+    emailCompra.style.border ="1px solid grey";
+    emailCompra.placeholder = "Correo";
+
 }
 
 
